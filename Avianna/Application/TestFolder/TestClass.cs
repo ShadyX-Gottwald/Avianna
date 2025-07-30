392f8fb9cd4f0e626a02c7686f8a12433afe758a
Shady <shadyxgottwald99@gmail.com>
2025-07-30 23:01:17 +0200

    Unit Test Folder Created

--- Avianna/Application/TestFolder/TestClass.cs
+++ Avianna/Application/TestFolder/TestClass.cs
@@ -3,4 +3,7 @@ namespace Avianna.Application.TestFolder;
 public class TestClass {
       public int ID { get; set; }
       public string Name { get; set; }
+
+      public DateTime Submit { get; set; }
+
 }
